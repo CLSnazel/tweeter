2ee1429ea8820fb7eac2b76cfa95b5bbc8527a79
Chantal Snazel <clsnazel@gmail.com>
2020-10-06 20:10:21 +0000

    added char counter, w/ class & script

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+const TWEETLENGTH = 140;
+$(document).ready(function() {
+  console.log('body is ready');
+  $('.new-tweet form > textarea').on("keydown", function() {
+    console.log(this);
+    console.log($(this).val().length);
+    let currLength = $(this).val().length;
+    let currRemain = TWEETLENGTH - currLength;
+
+    let divSibElem = $(this).siblings()[1];
+    let counterElem = $(divSibElem).children(".counter");
+    console.log(divSibElem, counterElem);
+    $(counterElem).html(currRemain);
+    if (currRemain < 0) {
+      $(counterElem).addClass('invalid-length');
+    } else {
+      $(counterElem).removeClass('invalid-length');
+    }
+  });
+});
