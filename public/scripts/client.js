429d288c3635cd1b23aec7360b9070dd3b64d3fb
Chantal Snazel <clsnazel@gmail.com>
2020-10-08 03:39:16 +0000

    added nav link to new-tweet w/ scroll-to

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1602112211318
-  }
-];
-
 $(document).ready(function(){
   $($('.error')[0]).slideUp();
   const renderTweetResponse = function(){
@@ -145,7 +119,7 @@ $(document).ready(function(){
     );
   };
   loadTweets();
-  // renderTweets(data);
+
   
   //given a message and a element to add msgElem after, 
   //adds error message as <p> tag
@@ -163,12 +137,8 @@ $(document).ready(function(){
     return;
   };
 
-  
-
-  const $tweetForm = $($('section.new-tweet form')[0]);
-  
-
-  
+  //================FORM POST SUBMIT ==================//
+  const $tweetForm = $($('section.new-tweet form')[0]);  
   $tweetForm.on('submit', function(event) {
     
     event.preventDefault();
@@ -204,5 +174,15 @@ $(document).ready(function(){
       });
     }
   });
+
+  //===============NAV <A> SCROLL TO NEW TWEET ==============//
+  $('nav a').click(function() {
+    $('body, html').animate(
+      {
+        scrollTop: $('.new-tweet').offset().top - $('nav').height() - 20
+      }, 
+      700
+    );
+  })
 
 });
